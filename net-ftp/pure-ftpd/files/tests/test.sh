11033b520f67cda447d46a3c50c2d3a1298189e7
kiorky <devnull@localhost>
2007-06-04 23:36:42 +0200

    [svn]

--- net-ftp/pure-ftpd/files/tests/test.sh
+++ net-ftp/pure-ftpd/files/tests/test.sh
@@ -2,7 +2,10 @@
 t="/home/kiorky/bordel/ng/ukn-gdj/Poppoya.avi"
 mkdir tmp;
 pushd  tmp;
+
+
 HOST="192.168.0.8"
+PORT="556"
 
 tls_opts="set ftp:ssl-allow yes ;set ftp:ssl-allow-anonymous no ;set ftp:ssl-auth TLS ;set ftp:ssl-data-use-keys yes ;set ftp:ssl-force yes ;set ftp:ssl-protect-data yes ;set ftp:ssl-protect-fxp yes ;set ftp:ssl-protect-list yes ;set ftp:ssl-use-ccc yes ;set ssl:verify-certificate no" 
 clear_opts="set ftp:ssl-allow yes ;set ftp:ssl-allow-anonymous no ;set ftp:ssl-auth TLS ;set ftp:ssl-data-use-keys no ;set ftp:ssl-force no ;set ftp:ssl-protect-data no ;set ftp:ssl-protect-fxp no ;set ftp:ssl-protect-list no ;set ftp:ssl-use-ccc no"
@@ -11,20 +14,20 @@ ttls() {
 	lftp  \
 	-u narishma,$1 \
 	-e "$tls_opts;put $t  ../*;ls -l;mget *;exit" \
-	ftp://192.168.0.8:5556;
+	ftp://${HOST}:${PORT};
 }
 
 tclear() {
 	lftp  \
 	-u narishma,$1 \
 	-e "$clear_opts;put $t  ../*;ls -l;mget *;exit" \
-	ftp://192.168.0.8:5556;
+	ftp://${HOST}:${PORT};
 }
 atclear() {
 	lftp  \
 	-u narishma,$1 \
 	-e "$aclear_opts;put $t  ../*;ls -l;mget *;exit" \
-	ftp://192.168.0.8:5556;
+	ftp://${HOST}:${PORT};
 } 
 
 ttls    $@
